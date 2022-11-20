1d03807ddc0486b27d975e95c68aa4a8188261d0
MicroHub69 <msodq2018@gmail.com>
2022-11-20 01:17:06 +0100

    Test BTN

--- src/Pages/SuperAdmin/Estates/Estate.jsx
+++ src/Pages/SuperAdmin/Estates/Estate.jsx
@@ -126,6 +126,9 @@ const Estate = () => {
               >
                 Add New Estate
               </button>
+              <Link to="/admin">
+                <button className="oulined-btn">Test BTN</button>
+              </Link>
             </div>
 
             {/* RESULT FROM TAB */}
